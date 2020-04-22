1a79fd7f0b8dea5bd692d2c654222eb8a96ebb61
yash <yashdvn@gmail.com>
2020-04-22 20:55:44 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+Visit the below endpoints<br/>
+
+Login:<br/>
+http://YOUR_MACHINE_IP/android_login_api/login.php<br/><br/>
+
+Registration:<br/>
+http://YOUR_MACHINE_IP/android_login_api/register.php<br/><br/>
+
+Tutorial link:<br/>
+<a href=""></a>
